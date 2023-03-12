e47dbd7a38bfdd50833ab466c8199265ccd0996a
lyz <40293541+lingyz233@users.noreply.github.com>
2023-03-12 17:54:57 +0800

    Update tryfilemap.c
    .

--- examples/tryfilemap.c
+++ examples/tryfilemap.c
@@ -15,7 +15,8 @@ int
 main (void)
 {
   struct filemap fm;
-  filemap.open (&fm, "./numbers", O_RDWR|O_CREAT, MAP_SHARED, PROT_READ|PROT_WRITE);
+//_Bool (*open) (struct filemap * fmap, char * path, int fflag, int flags, int prot, int fmode, size_t hoped_size);
+  filemap.open (&fm, "./numbers", O_RDWR|O_CREAT, MAP_SHARED, PROT_READ|PROT_WRITE,0755,16);
   filemap.resize (&fm, 16);
   for (int i = 0; i < 16; i++)
     fm.d[i] = '0' + i % 10;
